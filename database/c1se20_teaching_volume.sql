15139b0d9308d8ab7cb94abad8804bee7e89bd5c
banhan14999 <huynhbanhan1491999@gmail.com>
2022-10-10 14:32:04 +0700

    change database

--- database/c1se20_teaching_volume.sql
+++ database/c1se20_teaching_volume.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 08, 2022 at 02:24 AM
+-- Generation Time: Oct 10, 2022 at 09:31 AM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
@@ -24,22 +24,22 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `departments`
+-- Table structure for table `department`
 --
 
-CREATE TABLE `departments` (
-  `Department_id` varchar(20) NOT NULL,
-  `Deparment_name` varchar(200) NOT NULL,
+CREATE TABLE `department` (
+  `IdDepartment` varchar(20) NOT NULL,
+  `DepartmentName` varchar(200) NOT NULL,
   `Description` varchar(250) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `departments`
+-- Dumping data for table `department`
 --
 
-INSERT INTO `departments` (`Department_id`, `Deparment_name`, `Description`, `created_at`, `updated_at`) VALUES
+INSERT INTO `department` (`IdDepartment`, `DepartmentName`, `Description`, `created_at`, `updated_at`) VALUES
 ('MIS', 'Management Information Systems', '', '2022-10-07 15:20:39', '2022-10-07 15:20:39'),
 ('NS', 'Network Security', '', '2022-10-07 15:20:39', '2022-10-07 15:20:39'),
 ('SE', 'Software Engineer', '', '2022-10-07 15:20:39', '2022-10-07 15:20:39');
@@ -51,8 +51,8 @@ INSERT INTO `departments` (`Department_id`, `Deparment_name`, `Description`, `cr
 --
 
 CREATE TABLE `role` (
-  `Role_id` int(11) NOT NULL,
-  `Role_name` varchar(150) NOT NULL,
+  `IdRole` int(11) NOT NULL,
+  `RoleName` varchar(150) NOT NULL,
   `Description` varchar(200) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
@@ -62,7 +62,7 @@ CREATE TABLE `role` (
 -- Dumping data for table `role`
 --
 
-INSERT INTO `role` (`Role_id`, `Role_name`, `Description`, `created_at`, `updated_at`) VALUES
+INSERT INTO `role` (`IdRole`, `RoleName`, `Description`, `created_at`, `updated_at`) VALUES
 (1, 'Admin', '', '2022-10-07 15:08:25', '2022-10-07 15:08:25'),
 (2, 'Dean', '', '2022-10-07 15:08:25', '2022-10-07 15:08:25'),
 (3, 'Head', 'Head of Department', '2022-10-07 15:08:25', '2022-10-07 15:08:25'),
@@ -71,14 +71,14 @@ INSERT INTO `role` (`Role_id`, `Role_name`, `Description`, `created_at`, `update
 -- --------------------------------------------------------
 
 --
--- Table structure for table `subjects`
+-- Table structure for table `subject`
 --
 
-CREATE TABLE `subjects` (
-  `Subject_id` int(11) NOT NULL,
+CREATE TABLE `subject` (
+  `IdSubject` int(11) NOT NULL,
   `Letter` varchar(20) NOT NULL,
   `Number` smallint(5) UNSIGNED NOT NULL,
-  `Subject_name` varchar(200) NOT NULL,
+  `SubjectName` varchar(200) NOT NULL,
   `Credit` tinyint(3) UNSIGNED NOT NULL,
   `Type` varchar(20) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
@@ -86,10 +86,10 @@ CREATE TABLE `subjects` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `subjects`
+-- Dumping data for table `subject`
 --
 
-INSERT INTO `subjects` (`Subject_id`, `Letter`, `Number`, `Subject_name`, `Credit`, `Type`, `created_at`, `updated_at`) VALUES
+INSERT INTO `subject` (`IdSubject`, `Letter`, `Number`, `SubjectName`, `Credit`, `Type`, `created_at`, `updated_at`) VALUES
 (1, 'CMU-CS', 252, 'Introduction to Network & Telecommunications Technology', 3, 'LEC', '2022-10-04 15:39:02', '2022-10-04 15:39:02'),
 (2, 'CMU-SE', 214, 'Requirements Engineering', 3, 'LEC', '2022-10-04 15:39:02', '2022-10-04 15:39:02'),
 (3, 'CMU-CS', 297, 'Đồ Án CDIO', 1, 'DIS', '2022-10-06 02:10:36', '2022-10-06 03:44:11'),
@@ -98,28 +98,28 @@ INSERT INTO `subjects` (`Subject_id`, `Letter`, `Number`, `Subject_name`, `Credi
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table `user`
 --
 
-CREATE TABLE `users` (
+CREATE TABLE `user` (
   `Username` varchar(80) NOT NULL,
   `Password` varchar(150) NOT NULL,
-  `Lecturer_id` bigint(20) UNSIGNED DEFAULT NULL,
-  `Firstname` varchar(150) NOT NULL,
-  `Lastname` varchar(150) NOT NULL,
-  `Faculty_id` varchar(20) DEFAULT NULL,
-  `Department_id` varchar(20) DEFAULT NULL,
-  `Role_id` int(11) DEFAULT NULL,
+  `IdLecturer` bigint(20) UNSIGNED DEFAULT NULL,
+  `FirstName` varchar(150) NOT NULL,
+  `LastName` varchar(150) NOT NULL,
+  `IdFaculty` varchar(20) DEFAULT NULL,
+  `IdDepartment` varchar(20) DEFAULT NULL,
+  `IdRole` int(11) DEFAULT NULL,
   `Remember_token` varchar(255) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `users`
+-- Dumping data for table `user`
 --
 
-INSERT INTO `users` (`Username`, `Password`, `Lecturer_id`, `Firstname`, `Lastname`, `Faculty_id`, `Department_id`, `Role_id`, `Remember_token`, `created_at`, `updated_at`) VALUES
+INSERT INTO `user` (`Username`, `Password`, `IdLecturer`, `FirstName`, `LastName`, `IdFaculty`, `IdDepartment`, `IdRole`, `Remember_token`, `created_at`, `updated_at`) VALUES
 ('dangvantruc', 'vantruc', 252666777002, 'Đặng Văn', 'Trúc', NULL, 'NS', 3, NULL, '2022-10-07 15:27:14', '2022-10-07 15:27:14'),
 ('huynhbanhan', 'banhan', NULL, 'Huỳnh Bá', 'Nhân', NULL, NULL, 1, NULL, '2022-10-07 15:27:14', '2022-10-07 15:27:14'),
 ('leanhkhanh', 'anhkhanh', 25211320802, 'Lê Anh', 'Khánh', NULL, 'SE', 3, NULL, '2022-10-07 15:27:14', '2022-10-07 15:27:14'),
@@ -134,30 +134,30 @@ INSERT INTO `users` (`Username`, `Password`, `Lecturer_id`, `Firstname`, `Lastna
 --
 
 --
--- Indexes for table `departments`
+-- Indexes for table `department`
 --
-ALTER TABLE `departments`
-  ADD PRIMARY KEY (`Department_id`);
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`IdDepartment`);
 
 --
 -- Indexes for table `role`
 --
 ALTER TABLE `role`
-  ADD PRIMARY KEY (`Role_id`);
+  ADD PRIMARY KEY (`IdRole`);
 
 --
--- Indexes for table `subjects`
+-- Indexes for table `subject`
 --
-ALTER TABLE `subjects`
-  ADD PRIMARY KEY (`Subject_id`),
+ALTER TABLE `subject`
+  ADD PRIMARY KEY (`IdSubject`),
   ADD UNIQUE KEY `Unique_Letter_Number` (`Letter`,`Number`) USING BTREE;
 
 --
--- Indexes for table `users`
+-- Indexes for table `user`
 --
-ALTER TABLE `users`
+ALTER TABLE `user`
   ADD PRIMARY KEY (`Username`),
-  ADD UNIQUE KEY `Lecturer_id` (`Lecturer_id`);
+  ADD UNIQUE KEY `Lecturer_id` (`IdLecturer`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -167,13 +167,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `role`
 --
 ALTER TABLE `role`
-  MODIFY `Role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `IdRole` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `subjects`
+-- AUTO_INCREMENT for table `subject`
 --
-ALTER TABLE `subjects`
-  MODIFY `Subject_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+ALTER TABLE `subject`
+  MODIFY `IdSubject` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
