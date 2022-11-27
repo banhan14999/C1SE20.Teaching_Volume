f855142c5949a7080083b9dc5574139f452c2199
banhan14999 <huynhbanhan1491999@gmail.com>
2022-11-27 22:03:36 +0700

    update Validate for create new class

--- backend/database/migrations/2022_10_31_164854_create__grading_exam_volume_table.php
+++ backend/database/migrations/2022_10_31_164854_create__grading_exam_volume_table.php
@@ -26,7 +26,9 @@ return new class extends Migration
             $table->decimal("CoefficientGradeExam", 10,2);
             $table->string("CategoryVolume", 20);
             $table->timestamps();
-            $table->primary(['Year', 'Semester', 'IdLecturer', 'IdSubject', 'Time', 'CategoryVolume'], 'PK_LEC_YEAR_SEM_SUB_TIME');
+            //$table->primary(['Year', 'Semester', 'IdLecturer', 'IdSubject', 'Time', 'CategoryVolume'], 'PK_LEC_YEAR_SEM_SUB_TIME');
+            $table->foreign('IdLecturer')->references('IdLecturer')->on('users');
+            $table->foreign('IdSubject')->references('IdSubject')->on('subjects');
         });
     }
 
