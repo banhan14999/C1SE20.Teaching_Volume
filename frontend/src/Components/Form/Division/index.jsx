e384cdbf71237b323b4829226c52d3a5e48cd7f7
Le Anh Khanh <leanhkhanh6540@gmail.com>
2022-12-30 09:37:22 +0700

    v...

--- frontend/src/Components/Form/Division/index.jsx
+++ frontend/src/Components/Form/Division/index.jsx
@@ -46,8 +46,9 @@ useEffect(() => {
 }, [classSubject, lecturer, classroom, classlecturer]);
 
   const opt = [
-    { value: "2022", label: "2021-2022" },
-    { value: "2023", label: "2022-2023" },
+    { value: "2021", label: "2021-2022" },
+    { value: "2022", label: "2022-2023" },
+    { value: "2023", label: "2023-2024" },
     { value: "2024", label: "2024-2025" },
   ];
   const hocki = [
